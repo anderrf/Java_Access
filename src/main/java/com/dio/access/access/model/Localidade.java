85c6c06a0123d66989765ebce6cce61e7257f7ea
anderrf <anderson_rocha_farias@outlook.com>
2021-07-24 18:47:50 -0300

    Start of model relationships, Usuario model

--- src/main/java/com/dio/access/access/model/Localidade.java
+++ src/main/java/com/dio/access/access/model/Localidade.java
@@ -2,6 +2,8 @@ package com.dio.access.access.model;
 
 import lombok.*;
 
+import javax.persistence.OneToMany;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -9,5 +11,8 @@ import lombok.*;
 @EqualsAndHashCode
 @Builder
 public class Localidade {
-
+    private Long id;
+    @OneToMany
+    private NivelAcesso nivelAcesso;
+    private String descricao;
 }
