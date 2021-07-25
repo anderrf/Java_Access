d4c499587ea83f6d2b6dfe9f8ed9b62df33f4a59
anderrf <anderson_rocha_farias@outlook.com>
2021-07-25 15:41:11 -0300

    BancoHoras model, database properties set

---
+++ src/main/java/com/dio/access/access/model/BancoHoras.java
@@ -0,0 +1,32 @@
+package com.dio.access.access.model;
+
+import lombok.*;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class BancoHoras {
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
+    @Embeddable
+    public class BancoHorasId implements Serializable{
+        private Long idBancoHoras;
+        private Long idMovimentacao;
+        private Long idUsuario;
+    }
+    @EmbeddedId
+    private BancoHorasId id;
+    private LocalDateTime dataTrabalhada;
+    private BigDecimal quantidadeHoras;
+    private BigDecimal saldoHoras;
+}
