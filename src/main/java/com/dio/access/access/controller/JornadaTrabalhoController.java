19148b625a8c54e0345e93a00697a773ae24ef8b
anderrf <anderson_rocha_farias@outlook.com>
2021-07-25 20:05:16 -0300

    Controller, service and repository classes for Jornada model

--- src/main/java/com/dio/access/access/controller/JornadaTrabalhoController.java
+++ src/main/java/com/dio/access/access/controller/JornadaTrabalhoController.java
@@ -1,16 +1,21 @@
 package com.dio.access.access.controller;
 
 import com.dio.access.access.model.JornadaTrabalho;
+import com.dio.access.access.service.JornadaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/jornada")
 public class JornadaTrabalhoController {
+    @Autowired
+    JornadaService jornadaService;
 
     @PostMapping
-    public JornadaTrabalho createJornada(){
-        return null;
+    public JornadaTrabalho createJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.saveJornada(jornadaTrabalho);
     }
 }
--- src/main/java/com/dio/access/access/service/JornadaService.java
+++ src/main/java/com/dio/access/access/service/JornadaService.java
@@ -1,7 +1,20 @@
 package com.dio.access.access.service;
 
+import com.dio.access.access.model.JornadaTrabalho;
+import com.dio.access.access.repository.JornadaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JornadaService {
+    JornadaRepository jornadaRepository;
+
+    @Autowired
+    public JornadaService(JornadaRepository jornadaRepository){
+        this.jornadaRepository = jornadaRepository;
+    }
+
+    public JornadaTrabalho saveJornada(JornadaTrabalho jornadaTrabalho){
+        return jornadaRepository.save(jornadaTrabalho);
+    }
 }
